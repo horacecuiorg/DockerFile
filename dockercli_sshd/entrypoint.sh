7e9570dd47e9a7453c9d2b104709a14593878253
cuiyinhu <cuiyinhu@foxmail.com>
2025-05-28 03:16:28 +0000

    dockercli_sshd

--- dockercli_sshd/entrypoint.sh
+++ dockercli_sshd/entrypoint.sh
@@ -5,6 +5,8 @@ USER_ID=${UID:-1000}
 GROUP_ID=${GID:-1000}
 USERNAME=${USERNAME:-dockeruser}
 GROUPNAME=${GROUPNAME:-$USERNAME}
+DOCKER_GID=${DOCKER_GID:-999}  # 默认 root 所属组 stat -c '%g' /var/run/docker.sock
+SSH_KEY_FILE="/tmp/id_rsa.pub"
 
 # ===== 处理组 =====
 EXISTING_GROUP=$(getent group "$GROUP_ID" | cut -d: -f1)
@@ -32,6 +34,18 @@ else
     useradd -m -u "$USER_ID" -g "$GROUP_ID" -s /bin/bash "$USERNAME"
 fi
 
+# Docker GID 映射组（可选 docker.sock 权限修复）
+if [ -n "$DOCKER_GID" ]; then
+    DOCKER_GROUP_NAME=dockerhost
+    if ! getent group "$DOCKER_GID" >/dev/null; then
+        groupadd -g "$DOCKER_GID" "$DOCKER_GROUP_NAME"
+    else
+        DOCKER_GROUP_NAME=$(getent group "$DOCKER_GID" | cut -d: -f1)
+    fi
+    usermod -aG "$DOCKER_GROUP_NAME" "$USERNAME"
+fi
+
+
 # ===== 添加到 sudo 组（如果未在组中） =====
 if ! id "$USERNAME" | grep -q '\bsudo\b'; then
     usermod -aG sudo "$USERNAME"
@@ -47,4 +61,16 @@ if [ ! -f "$SUDO_FILE" ] || ! grep -Fxq "$LINE" "$SUDO_FILE"; then
 fi
 
 # ===== 切换用户执行 =====
-exec sudo -u "$USERNAME" "$@"
+# exec sudo -u "$USERNAME" "$@"
+
+# 设置 SSH 公钥
+if [ -f "$SSH_KEY_FILE" ]; then
+    SSH_HOME="/home/$USERNAME/.ssh"
+    mkdir -p "$SSH_HOME"
+    cat "$SSH_KEY_FILE" > "$SSH_HOME/authorized_keys"
+    chmod 600 "$SSH_HOME/authorized_keys"
+    chmod 700 "$SSH_HOME"
+    chown -R "$USERNAME:$GROUPNAME" "$SSH_HOME"
+fi
+
+exec "$@"
